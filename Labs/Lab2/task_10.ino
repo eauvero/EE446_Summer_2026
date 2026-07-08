4b92500bee67c93e0db0a02b0e79d72d9127ac27
eauvero <eauvero@gmail.com>
2026-07-08 11:58:41 -0700

    Add files via upload
    lab_2_submission

---
+++ Labs/Lab2/task_10.ino
@@ -0,0 +1,209 @@
+#include <PDM.h>
+#include <Arduino_APDS9960.h>
+#include <Arduino_BMI270_BMM150.h>
+#include <math.h>
+#include <string.h>
+
+int SOUND_THRESHOLD = 50;
+int DARK_THRESHOLD = 20;
+float MOVING_THRESHOLD = 0.01;
+int NEAR_THRESHOLD = 100;
+
+float baselineMag = 0.0;
+
+short sampleBuffer[256];
+volatile int samplesRead = 0;
+
+int evaluate_sound(int mic) {
+
+    if (mic > SOUND_THRESHOLD) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int evaluate_dark(int clear) {
+
+    if (clear < DARK_THRESHOLD) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int evaluate_moving(float motion) {
+
+    if (abs(motion - baselineMag)  > MOVING_THRESHOLD) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int evaluate_near(int prox) {
+    if (prox < NEAR_THRESHOLD) {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+
+void report_raw(int mic, int clear, float motion, int prox) {
+    Serial.print("raw");
+    Serial.print(",");
+    Serial.print("mic=");
+    Serial.print(mic);
+    Serial.print(",");
+    Serial.print("clear=");
+    Serial.print(clear);
+    Serial.print(",");
+    Serial.print("motion=");
+    Serial.print(motion, 3);
+    Serial.print(",");
+    Serial.print("prox=");
+    Serial.println(prox);
+    return;
+}
+
+void report_flags(int sound, int dark, int moving, int near) {
+    Serial.print("flags");
+    Serial.print(",");
+    Serial.print("sound=");
+    Serial.print(sound);
+    Serial.print(",");
+    Serial.print("dark=");
+    Serial.print(dark);
+    Serial.print(",");
+    Serial.print("moving=");
+    Serial.print(moving);
+    Serial.print(",");
+    Serial.print("near=");
+    Serial.println(near);
+    return;
+}
+
+
+void evaluate(int mic, int clear, float motion, int prox){
+    int sound = evaluate_sound(mic);
+    int dark = evaluate_dark(clear);
+    int moving = evaluate_moving(motion);
+    int near = evaluate_near(prox);
+
+    int state_id = (sound << 3) | (dark << 2) | (moving << 1) | near;
+    const char* final_state = "";
+
+    switch(state_id) {
+        case 0: final_state = "QUIET_BRIGHT_STEADY_FAR"; break;   //0000
+        case 8: final_state = "NOISY_BRIGHT_STEADY_FAR"; break;   //1000
+        case 5: final_state = "QUIET_DARK_STEADY_NEAR"; break;     //0101
+        case 11: final_state = "NOISY_BRIGHT_MOVING_NEAR"; break; //1011
+    }
+
+    if (strlen(final_state) > 0) {
+        report_raw(mic, clear, motion, prox);
+        report_flags(sound, dark, moving, near);
+        Serial.print("state,");
+        Serial.println(final_state);
+    }
+
+    return;
+}
+
+
+void onPDMdata() {
+    int bytesAvailable = PDM.available();
+    PDM.read(sampleBuffer, bytesAvailable);
+    samplesRead = bytesAvailable / 2;
+}
+
+
+float mag_accel(float ax, float ay, float az){
+    float mag = 0.0;
+    mag += ax*ax;
+    mag += ay*ay;
+    mag += az*az;
+    return sqrtf(mag);
+}
+
+void setup() {
+    Serial.begin(115200);
+    delay(1500);
+    bool baseline_taken = false;
+    float ax, ay, az;
+
+    PDM.onReceive(onPDMdata);
+
+    if(!PDM.begin(1, 16000)) {
+        Serial.println("Failed to start PDM microphone.");
+        while (1);
+    }
+
+    if (!IMU.begin()) {
+        Serial.println("Failed to initialize IMU.");
+        while (1);
+    }
+
+    if(!APDS.begin()) {
+        Serial.println("Failed to initialize APDS9960 sensor.");
+        while(1);
+    }
+
+    
+
+    while(!baseline_taken){
+        delay(1000);
+        if (IMU.accelerationAvailable()) {
+            IMU.readAcceleration(ax, ay, az);
+            baselineMag = sqrtf(ax*ax + ay*ay + az*az);
+            baseline_taken = true;
+            Serial.print("Baseline acceleration measured");
+        }
+    }
+
+    Serial.println("Smart Workspace Situation Classifier started");
+}
+
+void loop() {
+    int mic;
+    int r, g, b, clear;
+    float ax, ay, az, motion;
+    int prox;
+
+    int rgb_ready = 0;
+    int accel_ready = 0;
+    int prox_ready = 0;
+
+    if (APDS.colorAvailable()) {
+        rgb_ready = 1;
+    }
+
+    if (IMU.accelerationAvailable()) {
+        accel_ready = 1;
+    }
+
+    if (APDS.proximityAvailable()) {
+        prox_ready = 1;
+    }
+
+    if ((samplesRead) && (rgb_ready == 1) && (accel_ready == 1) && (prox_ready == 1)) {
+        long sum = 0;
+        for (int i = 0; i < samplesRead; i++) {
+            sum += abs(sampleBuffer[i]);
+        }
+        mic = sum / samplesRead;
+        samplesRead = 0;
+
+        APDS.readColor(r, g, b, clear);
+        IMU.readAcceleration(ax, ay, az);
+        motion = mag_accel(ax, ay, az);
+        prox = APDS.readProximity();
+
+        //report_raw(mic, clear, motion, prox);
+        evaluate(mic, clear, motion, prox);
+    }
+ 
+    delay(1000);
+}
---
+++ Labs/Lab2/task_11.ino
@@ -0,0 +1,286 @@
+#include <Arduino_HS300x.h>
+#include <Arduino_BMI270_BMM150.h>
+#include <Arduino_APDS9960.h>
+#include <math.h>
+
+bool isInitialized = false;
+
+float ALPHA = 0.15;
+unsigned long last_baseline_TriggerTime = 0;
+unsigned long last_breathWarmAir_TriggerTime = 0;
+unsigned long last_magneticDisturbance_TriggerTime = 0;
+unsigned long last_lightColorChange_TriggerTime = 0;
+const unsigned long eventTriggerLockOutDuration = 3000;
+const unsigned long baselineTriggerLockOutDuration = 6000;
+
+
+float RH_THRESHOLD = 1.0;
+float TEMP_THRESHOLD = 0.2;
+float MAG_THRESHOLD = 1.5;
+float R_THRESHOLD = 1.0;
+float G_THRESHOLD = 1.0;
+float B_THRESHOLD = 1.0;
+float CLEAR_THRESHOLD = 1.0;
+
+
+float prev_avg_rh, prev_avg_temp;
+float prev_avg_mag_x, prev_avg_mag_y, prev_avg_mag_z;
+float prev_avg_r, prev_avg_g, prev_avg_b, prev_avg_clear;
+float curr_avg_rh, curr_avg_temp;
+float curr_avg_mag_x, curr_avg_mag_y, curr_avg_mag_z;
+float curr_avg_r, curr_avg_g, curr_avg_b, curr_avg_clear;
+
+
+float magnitude(float x, float y, float z) {
+    float mag = 0.0;
+    mag += x*x;
+    mag += y*y;
+    mag += z*z;
+
+    return sqrt(mag);
+}
+
+
+void report_raw(float rh, float temp, float mag, int r, int g, int b, int clear) {
+    Serial.print("raw");
+    Serial.print(",");
+    Serial.print("rh=");
+    Serial.print(rh, 2);
+    Serial.print(",");
+    Serial.print("temp=");
+    Serial.print(temp, 2);
+    Serial.print(",");
+    Serial.print("mag=");
+    Serial.print(mag, 3);
+    Serial.print(",");
+    Serial.print("r=");
+    Serial.print(r);
+    Serial.print(",");
+    Serial.print("g=");
+    Serial.print(g);
+    Serial.print(",");
+    Serial.print("b=");
+    Serial.print(b);
+    Serial.print(",");
+    Serial.print("clear=");
+    Serial.println(clear);
+    return;
+}
+
+void report_flags(int humid_jump, int temp_rise, int mag_shift, int light_or_color_change) {
+    Serial.print("flags");
+    Serial.print(",");
+    Serial.print("humid_jump=");
+    Serial.print(humid_jump);
+    Serial.print(",");
+    Serial.print("temp_rise=");
+    Serial.print(temp_rise);
+    Serial.print(",");
+    Serial.print("mag_shift=");
+    Serial.print(mag_shift);
+    Serial.print(",");
+    Serial.print("light_or_color_change=");
+    Serial.println(light_or_color_change);
+    return;
+}
+
+
+int process_rh_temp(unsigned long currentTime, float raw_rh, float raw_temp, float raw_mag, int raw_r, int raw_g, int raw_b, int raw_clear){
+
+    int humid_jump = 0;
+    int temp_rise = 0;
+
+    prev_avg_rh = curr_avg_rh;
+    prev_avg_temp = curr_avg_temp;
+
+    curr_avg_rh = (ALPHA * raw_rh) + ((1.0 - ALPHA) * curr_avg_rh);
+    curr_avg_temp = (ALPHA * raw_temp) + ((1.0 - ALPHA) * curr_avg_temp);
+
+    if (curr_avg_rh - prev_avg_rh > RH_THRESHOLD) {
+        humid_jump = 1;
+    }
+
+    if (curr_avg_temp - prev_avg_temp > TEMP_THRESHOLD) {
+        temp_rise = 1;
+    }
+
+
+    if ((humid_jump == 1) || (temp_rise == 1)) {
+        if (last_breathWarmAir_TriggerTime == 0 || (currentTime - last_breathWarmAir_TriggerTime >= eventTriggerLockOutDuration)) {
+            report_raw(raw_rh, raw_temp, raw_mag, raw_r, raw_g, raw_b, raw_clear);
+            report_flags(humid_jump, temp_rise, 0, 0);
+            Serial.println("event,BREATH_OR_WARM_AIR_EVENT");
+            last_breathWarmAir_TriggerTime = currentTime;
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    return 0;
+}
+
+int process_mag(unsigned long currentTime, float raw_rh, float raw_temp, float raw_mag, int raw_r, int raw_g, int raw_b, int raw_clear, float raw_mag_x, float raw_mag_y, float raw_mag_z){
+
+    float prev_avg_mag, curr_avg_mag;
+
+    prev_avg_mag = magnitude(prev_avg_mag_x, prev_avg_mag_y, prev_avg_mag_z);
+
+    prev_avg_mag_x = curr_avg_mag_x;
+    prev_avg_mag_y = curr_avg_mag_y;
+    prev_avg_mag_z = curr_avg_mag_z;
+
+    curr_avg_mag_x = (ALPHA * raw_mag_x) + ((1.0 - ALPHA) * curr_avg_mag_x);
+    curr_avg_mag_y = (ALPHA * raw_mag_y) + ((1.0 - ALPHA) * curr_avg_mag_y);
+    curr_avg_mag_z = (ALPHA * raw_mag_z) + ((1.0 - ALPHA) * curr_avg_mag_z);
+
+    curr_avg_mag = magnitude(curr_avg_mag_x, curr_avg_mag_y, curr_avg_mag_z);
+
+    if (fabs(curr_avg_mag - prev_avg_mag) > MAG_THRESHOLD) {
+        if (last_magneticDisturbance_TriggerTime == 0 || (currentTime - last_magneticDisturbance_TriggerTime >= eventTriggerLockOutDuration)) {
+            report_raw(raw_rh, raw_temp, raw_mag, raw_r, raw_g, raw_b, raw_clear);
+            report_flags(0, 0, 1, 0);
+            Serial.println("event,MAGNETIC_DISTURBANCE_EVENT");
+            last_magneticDisturbance_TriggerTime = currentTime;
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+int process_rgb_clear(unsigned long currentTime, float raw_rh, float raw_temp, float raw_mag, int raw_r, int raw_g, int raw_b, int raw_clear){
+    prev_avg_r = curr_avg_r;
+    prev_avg_g = curr_avg_g;
+    prev_avg_b = curr_avg_b;
+    prev_avg_clear = curr_avg_clear;
+
+    curr_avg_r = (ALPHA * raw_r) + ((1.0 - ALPHA) * curr_avg_r);
+    curr_avg_g = (ALPHA * raw_g) + ((1.0 - ALPHA) * curr_avg_g);
+    curr_avg_b = (ALPHA * raw_b) + ((1.0 - ALPHA) * curr_avg_b);
+    curr_avg_clear = (ALPHA * raw_clear) + ((1.0 - ALPHA) * curr_avg_clear);
+
+    if ((fabs(curr_avg_r - prev_avg_r) > R_THRESHOLD)
+      || (fabs(curr_avg_g - prev_avg_g) > G_THRESHOLD)
+      || (fabs(curr_avg_b - prev_avg_b) > B_THRESHOLD)
+      || (fabs(curr_avg_clear - prev_avg_clear) > CLEAR_THRESHOLD)
+    ) {
+        if (last_lightColorChange_TriggerTime == 0 || (currentTime - last_lightColorChange_TriggerTime >= eventTriggerLockOutDuration)) {
+            report_raw(raw_rh, raw_temp, raw_mag, raw_r, raw_g, raw_b, raw_clear);
+            report_flags(0, 0, 0, 1);
+            Serial.println("event,LIGHT_OR_COLOR_CHANGE_EVENT");
+            last_lightColorChange_TriggerTime = currentTime;
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+void evaluate(float raw_rh, float raw_temp, float raw_mag_x, float raw_mag_y, float raw_mag_z, int raw_r, int raw_g, int raw_b, int raw_clear){
+    float raw_mag;
+    int breath_or_warm_air = 0;
+    int magnetic_disturbance_event = 0;
+    int light_or_color_change_event = 0;
+
+    raw_mag = magnitude(raw_mag_x, raw_mag_y, raw_mag_z);
+
+    unsigned long currentTime = millis();
+
+    breath_or_warm_air = process_rh_temp(currentTime, raw_rh, raw_temp, raw_mag, raw_r, raw_g, raw_b, raw_clear);
+    magnetic_disturbance_event = process_mag(currentTime, raw_rh, raw_temp, raw_mag, raw_r, raw_g, raw_b, raw_clear, raw_mag_x, raw_mag_y, raw_mag_z);
+    light_or_color_change_event = process_rgb_clear(currentTime, raw_rh, raw_temp, raw_mag, raw_r, raw_g, raw_b, raw_clear);
+
+    if((breath_or_warm_air == 0) && (magnetic_disturbance_event == 0) && (light_or_color_change_event == 0)) {
+        if (last_baseline_TriggerTime == 0 || (currentTime - last_baseline_TriggerTime >= baselineTriggerLockOutDuration)) {
+            report_raw(raw_rh, raw_temp, raw_mag, raw_r, raw_g, raw_b, raw_clear);
+            report_flags(0, 0, 0, 0);
+            Serial.println("event,BASELINE_NORMAL");
+            last_baseline_TriggerTime = currentTime;
+        }
+    }
+}
+
+
+void setup() {
+    Serial.begin(115200);
+    delay(1500);
+
+    if(!HS300x.begin()) {
+        Serial.println("Failed to initialize humidity/temperature sensor.");
+        while(1);
+    }
+
+    if(!IMU.begin()) {
+        Serial.println("Failed to initialize IMU.");
+        while (1);
+    }
+
+    if(!APDS.begin()) {
+        Serial.println("Failed to initialize APDS9960 sensor.");
+        while(1);
+    }
+
+    Serial.println("Indoor Environment Change Detector started");
+
+    delay(1000);
+
+}
+
+void loop() {
+    float rh, temp;
+    float mag_x, mag_y, mag_z;
+    int   r, g, b, clear;
+
+    int   mag_ready = 0;
+    int   rgb_ready = 0;
+
+
+    if (IMU.magneticFieldAvailable()) {
+        mag_ready = 1;
+    }
+
+    if (APDS.colorAvailable()) {
+        rgb_ready = 1;
+    }
+
+    if ((mag_ready == 1) && (rgb_ready == 1)) {
+        temp = HS300x.readTemperature();
+        rh = HS300x.readHumidity();
+        IMU.readMagneticField(mag_x, mag_y, mag_z);
+        APDS.readColor(r, g, b, clear);
+
+        if (!isInitialized) {
+            prev_avg_rh = rh;
+            prev_avg_temp = temp;
+            curr_avg_rh = rh;
+            curr_avg_temp = temp;
+
+            prev_avg_mag_x = mag_x;
+            prev_avg_mag_y = mag_y;
+            prev_avg_mag_z = mag_z;
+            curr_avg_mag_x = mag_x;
+            curr_avg_mag_y = mag_y;
+            curr_avg_mag_z = mag_z;
+
+            prev_avg_r = (float) r;
+            prev_avg_g = (float) g;
+            prev_avg_b = (float) b;
+            prev_avg_clear = (float) clear;
+            curr_avg_r = (float) r;
+            curr_avg_g = (float) g;
+            curr_avg_b = (float) b;
+            curr_avg_clear = (float) clear;
+
+            isInitialized = true;
+        }
+
+        evaluate(rh, temp, mag_x, mag_y, mag_z, r, g, b, clear);
+
+    }
+
+    delay(100);
+}
